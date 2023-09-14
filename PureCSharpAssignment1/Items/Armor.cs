08dea7f42c1fd257ec7609bdb92f1444b2e4e3e6
Pau Go Si <paugosi@hotmail.com>
2023-09-14 10:32:41 +0200

    Added properties and constructor to the Armor class

--- PureCSharpAssignment1/Items/Armor.cs
+++ PureCSharpAssignment1/Items/Armor.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PureCSharpAssignment1.Enums;
+using PureCSharpAssignment1.Heros;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,17 @@ namespace PureCSharpAssignment1.Items
 {
     public class Armor : Item
     {
-        public Armor() { }
+        //A ArmorType property that will store the type of the armor.
+        public ArmorType ArmorType { get; set; }
+
+        //A HeroAttribute property that will store the type of the HeroAttribute
+        public HeroAttribute HeroAttribute { get; private set; }
+
+        //A constructor that initializes these properties.
+        public Armor(ArmorType armorType, HeroAttribute armorAttribute)
+        {
+            ArmorType = armorType;
+            HeroAttribute = armorAttribute;
+        }
     }
 }

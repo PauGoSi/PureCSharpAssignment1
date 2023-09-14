bc3edb5f41d0e18464dd469b7a033ead48b838b6
Pau Go Si <paugosi@hotmail.com>
2023-09-14 09:26:08 +0200

    Added properties and constructor to the Weapon class

--- PureCSharpAssignment1/Items/Weapon.cs
+++ PureCSharpAssignment1/Items/Weapon.cs
@@ -4,6 +4,17 @@ namespace PureCSharpAssignment1.Items
 {
     public class Weapon : Item
     {
-        
+        //A WeaponType property that will store the type of the weapon.
+        public WeaponType WeaponType { get; set; }
+
+        //A Damage property that will store the damage value of the weapon.
+        public int WeaponDamage { get; set; } // Weapons will have a damage value
+
+        //A constructor that initializes these properties.
+        public Weapon(WeaponType weaponType, int weaponDamage)
+        {
+            WeaponType = weaponType;
+            WeaponDamage = weaponDamage;
+        }
     }
 }

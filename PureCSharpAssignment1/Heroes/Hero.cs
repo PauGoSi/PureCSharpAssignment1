9cdcfe146d218bc7be2d6ff03bb4bcfd4c1eed20
Pau Go Si <paugosi@hotmail.com>
2023-09-14 12:49:30 +0200

    Added InvalidWeaponException

--- PureCSharpAssignment1/Heroes/Hero.cs
+++ PureCSharpAssignment1/Heroes/Hero.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//using PureCSharpAssignment1.Exceptions;
 
 
 namespace PureCSharpAssignment1.Heros
@@ -16,7 +17,10 @@ namespace PureCSharpAssignment1.Heros
         // A Name property that will store the name of the hero.
         protected string Name { get; set; }
 
-        // 
+        // Using protected below makes sense for properties like Name, Level, and others because
+        // we might want subclasses (Wizard, Archer, etc.) to have direct access to these fields or properties.
+        // This facilitates easier manipulation and checking of these fields directly in the subclass methods without
+        // the need for additional public getter or setter methods.
         protected int Level { get; set; } = 1;  // All heroes start at level 1
         protected HeroAttribute LevelAttributes { get; set; }
         protected Dictionary<Slot, Item> Equipment { get; set; }
@@ -42,7 +46,29 @@ namespace PureCSharpAssignment1.Heros
             // This would usually be defined in the subclass
         }
 
+        public void EquipWeapon(Weapon weapon)
+        {
+            // Check if the weapon type is valid for this hero
+            if (!ValidWeaponTypes.Contains(weapon.WeaponType))
+                throw new InvalidExceptions.InvalidWeaponException("This hero cannot equip this weapon type!");
 
+            // Equip the weapon
+            Equipment[Slot.Weapon] = weapon;
+        }
 
+        /*
+        public void EquipArmor(Armor armor)
+        {
+            // Check if the armor type is valid for this hero
+            if (!ValidArmorTypes.Contains(armor.ArmorType))
+                throw new InvalidArmorException("This hero cannot equip this armor type!");
+
+            // Equip the armor to the respective slot
+            Equipment[armor.Slot] = armor;
+        }
+
+        public abstract int Damage();  // To be implemented by subclasses
+        public abstract HeroAttribute TotalAttributes();  // To be implemented by subclasses
+        */
     }
 }
---
+++ PureCSharpAssignment1/InvalidExceptions/InvalidWeaponException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PureCSharpAssignment1.InvalidExceptions
+{
+    public class InvalidWeaponException : Exception
+    {
+        public InvalidWeaponException() { }
+        public InvalidWeaponException(string message) : base(message) { }
+
+    }
+    
+}

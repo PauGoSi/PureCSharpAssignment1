c3433bc9e5a2e0d91ad7aae3098a9573cf89068b
Pau Go Si <paugosi@hotmail.com>
2023-09-15 21:16:25 +0200

    Added calculated damage test for hero

--- Tests/DamageCalculationTests.cs
+++ Tests/DamageCalculationTests.cs
@@ -56,5 +56,52 @@ namespace PureCSharpAssignment1.Tests
             // Assert
             Assert.Equal(expectedDamage, actualDamage);
         }
+
+        [Fact]
+        public void HeroBarbarianDamage_NoWeaponEquipped_ShouldBeCorrect()
+        {
+            // Arrange
+            var barbarian = new Barbarian("Conan");
+
+            // Expected damage = 1 * (1 + (5/100)) = 1.05 (rounded to nearest integer)
+            int expectedDamage = 1;
+
+            // Assert
+            Assert.Equal(expectedDamage, barbarian.Damage());
+        }
+
+        [Fact]
+        public void HeroBarbarianDamage_WithWeaponEquipped_ShouldBeCorrect()
+        {
+            // Arrange
+            var barbarian = new Barbarian("Conan");
+            var hatchet = new Weapon("Common Hatchet", 1, WeaponType.HATCHETS, 2);
+            barbarian.EquipWeapon(hatchet);
+
+            // Expected damage = 2 * (1 + (5/100)) = 2.1 (rounded to nearest integer)
+            int expectedDamage = 2;
+
+            // Assert
+            Assert.Equal(expectedDamage, barbarian.Damage());
+        }
+
+        [Fact]
+        public void HeroBarbarianDamage_WithWeaponAndArmorEquipped_ShouldBeCorrect()
+        {
+            // Arrange
+            var barbarian = new Barbarian("Conan");
+            var hatchet = new Weapon("Common Hatchet", 1, WeaponType.HATCHETS, 2);
+            var armorAttribute = new HeroAttribute(1, 0, 0);
+            var plateArmor = new Armor("Common Plate Chest", 1, Slot.Body, ArmorType.Plate, armorAttribute);
+            
+            barbarian.EquipWeapon(hatchet);
+            barbarian.EquipArmor(plateArmor);
+
+            // Expected damage = 2 * (1 + ((5+1)/100)) = 2.12 (rounded to nearest integer)
+            int expectedDamage = 2;
+
+            // Assert
+            Assert.Equal(expectedDamage, barbarian.Damage());
+        }
     }
 }
---
+++ Tests/ItemTests.cs
@@ -0,0 +1,39 @@
+﻿using PureCSharpAssignment1.Enums;
+using PureCSharpAssignment1.Heroes;
+using PureCSharpAssignment1.Items;
+using Xunit;
+
+namespace PureCSharpAssignment1.Tests
+{
+    public class ItemTests
+    {
+        [Fact]
+        public void Weapon_ShouldHaveCorrectState()
+        {
+            // Arrange
+            var weapon = new Weapon("Common Hatchet", 1, WeaponType.HATCHETS, 2);
+
+            // Assert
+            Assert.Equal("Common Hatchet", weapon.Name);
+            Assert.Equal(1, weapon.RequiredLevel);
+            Assert.Equal(Slot.Weapon, weapon.Slot);
+            Assert.Equal(WeaponType.HATCHETS, weapon.WeaponType);
+            Assert.Equal(2, weapon.WeaponDamage);
+        }
+
+        [Fact]
+        public void Armor_ShouldHaveCorrectState()
+        {
+            // Arrange
+            var armorAttribute = new HeroAttribute(1, 0, 0);
+            var armor = new Armor("Common Plate Chest", 1, Slot.Body, ArmorType.Plate, armorAttribute);
+
+            // Assert
+            Assert.Equal("Common Plate Chest", armor.Name);
+            Assert.Equal(1, armor.RequiredLevel);
+            Assert.Equal(Slot.Body, armor.Slot);
+            Assert.Equal(ArmorType.Plate, armor.ArmorType);
+            Assert.Equal(armorAttribute, armor.ArmorAttribute);
+        }
+    }
+}
